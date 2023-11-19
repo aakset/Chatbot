f4fc66890f4a648242bea1dc3f2c5f8be0f3a8bf
aakashkbw <114473332+aakashkbw@users.noreply.github.com>
2023-11-19 13:21:49 +0100

    Verbindung zu backend + Login seite + CSS gefixt

--- chatbot-backend/index.js
+++ chatbot-backend/index.js
@@ -1,42 +1,47 @@
-// index.js
-
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 const PORT = 3001;
 
 app.use(bodyParser.json());
-app.use(cors()); // Enable CORS for all routes
+app.use(cors());
 
 const users = [];
 
-// Endpoint to register a user
-app.post('/register', (req, res) => {
+app.post("/register", (req, res) => {
   const { username } = req.body;
 
-  // Check if the username is already taken
   if (users.includes(username)) {
-    return res.status(400).json({ success: false, message: 'Username already taken' });
+    return res
+      .status(400)
+      .json({ success: false, message: "Username already taken" });
   } else {
     users.push(username);
-    return res.status(201).json({ success: true, message: 'User registered successfully' });
+    return res
+      .status(201)
+      .json({ success: true, message: "User registered successfully" });
   }
 });
 
-// Endpoint to handle login
-app.post('/login', (req, res) => {
+app.post("/login", (req, res) => {
   const { username } = req.body;
 
-  // Check if the username exists
   if (users.includes(username)) {
-    return res.status(200).json({ success: true, message: 'Login successful!' });
+    return res
+      .status(200)
+      .json({ success: true, message: "Login successful!" });
   } else {
-    return res.status(401).json({ success: false, message: 'Invalid username' });
+    return res
+      .status(401)
+      .json({ success: false, message: "Invalid username" });
   }
 });
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+//ohne CORS gehts nicht!
+
